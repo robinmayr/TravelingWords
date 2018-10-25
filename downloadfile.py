ddf141b2aaa8570a53e11dcc90220c468f384faf
Robin <robinmayr@gmail.com>
2018-10-25 20:26:42 +0200

    added scripts_to_db function

--- downloadfile.py
+++ downloadfile.py
@@ -1,81 +1,89 @@
-
 import os
-import shutil
-import string
-import subprocess
 from io import TextIOWrapper
 
 import lupa
-import requests
 import sqlalchemy
 from lxml import etree
 
-wiki_namespace = '{http://www.mediawiki.org/xml/export-0.10/}'
-database_name = 'traveling_word_pages.db'
-xml_dump_filename = 'enwiktionary-20180720-pages-meta-current.xml'
-engine = sqlalchemy.create_engine('sqlite:///' + database_name)
+WIKI_NAMESPACE = '{http://www.mediawiki.org/xml/export-0.10/}'
+DATABASE_NAME = 'traveling_word_pages.db'
+XML_DUMP_FILENAME = 'enwiktionary-20180720-pages-meta-current.xml'
+ENGINE = sqlalchemy.create_engine('sqlite:///' + DATABASE_NAME)
 
-metadata = sqlalchemy.MetaData()
+METADATA = sqlalchemy.MetaData()
 
-pages = sqlalchemy.Table('pages', metadata,
+PAGES = sqlalchemy.Table(
+    'pages',
+    METADATA,
     sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),
     sqlalchemy.Column('title', sqlalchemy.String, index=True, nullable=False),
     sqlalchemy.Column('namespace', sqlalchemy.Integer, index=True, nullable=False),
     sqlalchemy.Column('text', sqlalchemy.Text, nullable=False)
 )
 
+SCRIPTS = sqlalchemy.Table(
+    'scripts',
+    METADATA,
+    sqlalchemy.Column('code', sqlalchemy.String, primary_key=True),
+    sqlalchemy.Column('canonicalName', sqlalchemy.String, index=True, nullable=False),
+    sqlalchemy.Column('character_category', sqlalchemy.Boolean, index=True, nullable=False, default=True),
+    sqlalchemy.Column('characters', sqlalchemy.String),
+    sqlalchemy.Column('direction', sqlalchemy.String, index=True),
+    sqlalchemy.Column('otherNames', sqlalchemy.JSON),
+    sqlalchemy.Column('systems', sqlalchemy.JSON)
+)
+
 def page_elements(f: TextIOWrapper) -> etree.Element:
     in_page = False
     for ev, el in etree.iterparse(f, ('start', 'end')):
         if ev == 'start':
-            if el.tag == wiki_namespace + 'page':
+            if el.tag == WIKI_NAMESPACE + 'page':
                 in_page = True
         else:
-            if el.tag == wiki_namespace + 'page':
+            if el.tag == WIKI_NAMESPACE + 'page':
                 yield el
                 el.clear()
                 in_page = False
             else:
                 if not in_page:
                     el.clear()
-            
+ 
 def xml_to_db(breakcount=10000) -> None:
-    metadata.create_all(engine)
+    METADATA.create_all(ENGINE)
     page_buffer = []
-    connection = engine.connect()
+    connection = ENGINE.connect()
     mistakes = 0
-    with open(xml_dump_filename, 'rb') as f:
-        for i, el in enumerate(page_elements(f), 1):
-            page = create_from_element(el)
+    with open(XML_DUMP_FILENAME, 'rb') as f:
+        for i, element in enumerate(page_elements(f), 1):
+            page = create_from_element(element)
             if page['text']:
                 page_buffer.append(page)
-            
             if not i % breakcount:
-                connection.execute(pages.insert(page_buffer))
+                connection.execute(PAGES.insert(page_buffer))
                 print(f'pages loaded: {i},  last one: {page["title"]}')
                 page_buffer = []
-        connection.execute(pages.insert(page_buffer))
+        connection.execute(PAGES.insert(page_buffer))
         print(f'mistakes: {mistakes}')
 
 def create_from_element(el: etree.Element) -> dict:
     for child in el:
-        if child.tag == wiki_namespace + 'title':
+        if child.tag == WIKI_NAMESPACE + 'title':
             title = child.text
-        elif child.tag == wiki_namespace + 'ns':
+        elif child.tag == WIKI_NAMESPACE + 'ns':
             ns = child.text
-        elif child.tag == wiki_namespace + 'id':
+        elif child.tag == WIKI_NAMESPACE + 'id':
             page_id = int(child.text)
-        elif child.tag == wiki_namespace + 'revision':
+        elif child.tag == WIKI_NAMESPACE + 'revision':
             for grandchild in child:
-                if grandchild.tag == wiki_namespace + 'text':
+                if grandchild.tag == WIKI_NAMESPACE + 'text':
                     text = grandchild.text
         return {'id': page_id, 'title': title, 'namespace': ns, 'text': text}
 
 def extract_modules():
-    s = sqlalchemy.select([pages.c.title,pages.c.text]) \
-        .where(pages.c.namespace==828) \
-        .where(pages.c.title.notlike('%/documentation'))
-    for row in engine.connect().execute(s):
+    s = sqlalchemy.select([PAGES.c.title, PAGES.c.text]) \
+        .where(PAGES.c.namespace == 828) \
+        .where(PAGES.c.title.notlike('%/documentation'))
+    for row in ENGINE.connect().execute(s):
         file_path = 'Module/' + row['title'] + '.lua'
         directory = os.path.dirname(file_path)
         if not os.path.exists(directory):
@@ -108,11 +116,20 @@ def luatable_to_python(luatable):
             return luatable_to_dict(luatable)
     return luatable_to_list(luatable)
 
-def list_scripts() -> dict:
+def list_scripts() -> list:
     lua = lupa.LuaRuntime()
     with open('Module2/mw.lua') as mw_file, \
             open('Module2/Module:scripts/data.lua') as script_file:
         lua.execute(mw_file.read())
         program = script_file.read()
         luatable = lua.execute(program)
-        return luatable_to_dict(luatable)
+        scripts_as_dict = luatable_to_dict(luatable)
+        for key, value in scripts_as_dict.items():
+            value['code'] = key
+        return scripts_as_dict.values
+
+def scripts_to_db() -> None:
+    scripts = list_scripts()
+    METADATA.create_all(ENGINE)
+    connection = ENGINE.connect()
+    connection.execute(SCRIPTS.insert(scripts))
